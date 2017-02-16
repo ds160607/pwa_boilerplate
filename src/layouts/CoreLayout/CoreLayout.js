ef7407088752011cf431c787f3a4e08e84af3742
dgrushin <ds160607@mail.ru>
2017-02-16 07:57:17 +0200

    hide Drawer when routing

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -3,20 +3,29 @@ import { Link } from 'react-router'
 import Header from '../../components/Header'
 import './CoreLayout.scss'
 import '../../styles/core.scss'
-import { Layout, Navigation, Drawer, Content } from 'react-mdl';
+import { Layout, Navigation, Drawer, Content } from 'react-mdl'
+
+const onRouting = () => {
+  let layout = document.querySelector('.mdl-layout')
+  layout.MaterialLayout.toggleDrawer()
+}
+
+const drawer = (
+  <Drawer title='Title'>
+    <Navigation>
+      <Link to='/' onClick={onRouting}>main</Link>
+      <Link to='/counter' onClick={onRouting}>counter</Link>
+    </Navigation>
+  </Drawer>
+)
 
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
     <Layout fixedHeader >
       <Header />
-      <Drawer title='Title'>
-        <Navigation>
-          <Link to="/">main</Link>
-          <Link to="/counter">counter</Link>
-        </Navigation>
-      </Drawer>
+      { drawer }
       <Content>
-          {children}
+        {children}
       </Content>
     </Layout>
   </div>
